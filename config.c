1d7248d9903c8d3701f243e80081b556a1ba0a8b
d <you@example.com>
2022-01-10 22:41:32 +0100

    modkeys

--- config.c
+++ config.c
@@ -58,8 +58,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
-#define ALTKEY Mod1Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
