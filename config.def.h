1a802cee4b343c978c0b46e2d17f74d9ef7664b5
d <you@example.com>
2022-01-12 21:50:04 +0100

    customize config.def

--- config.def.h
+++ config.def.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,10 +60,16 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *mailcmd[] = {"thunderbird", NULL}; // FIXME
+static const char *qjackctlcmd[] = {"qjackctl", NULL};
+static const char *browsecmd[] = {"firefox", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_p,      focusmaster,    {0} },
+	{ ALTKEY,                       XK_b,      spawn,          {.v = browsecmd } },
+	{ ALTKEY,                       XK_q,      spawn,          {.v = qjackctlcmd } },
+	{ ALTKEY,                       XK_t,      spawn,          {.v = mailcmd } }, // FIXME
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
