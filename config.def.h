d0ce109d9c6e09e7b33e1be8f021b88ae0bc03c1
d <you@example.com>
2022-01-12 15:50:52 +0100

    shortkey

--- config.def.h
+++ config.def.h
@@ -63,6 +63,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ControlMask,           XK_p,      focusmaster,    {0} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
